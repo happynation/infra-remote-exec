6b03c11c71c6a23c2c3a32ed183c110c4200fccd
vovatran1993 <vovatran1993@gmail.com>
2019-08-07 19:13:55 -0500

    fixed

--- sec_group.tf
+++ sec_group.tf
@@ -9,24 +9,25 @@ resource "aws_security_group" "jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-egress {
+
+  egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
-  
 
-    }
-    tags = {
-      Name = "${var.Name}"
+  tags = {
+      Name = "${var.Name}.public"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
+}
